9186fb4aad8a49fde9aa25c3d87d94f478330429
Roberto Alvarez <robertoatc02@gmail.com>
2014-05-26 17:52:31 -0700

    Add native menu option/upload button

---
+++ app/js/node-main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var gui = require('nw.gui');
+  var win = gui.Window.get();
+  var menubar = new gui.Menu({ type: 'menubar' });
+  var file = new gui.Menu();
+  var help = new gui.Menu();
+  win.menu = menubar;
+  win.menu.insert(new gui.MenuItem({ label: 'File', submenu: file}), 1);
+  win.menu.append(new gui.MenuItem({ label: 'Help', submenu: help}));
+
