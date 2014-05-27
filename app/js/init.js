3bc0948a8e969bc20f7f2537ffccf4699963d035
Roberto Alvarez <robertoatc02@gmail.com>
2014-05-26 21:01:22 -0700

    Shows drop down menu for browsing files

--- app/js/init.js
+++ app/js/init.js
@@ -1,10 +1,39 @@
 "use strict";
+// var fs = require('fs');
+// var path = require('path');
+// console.log('---')
+// var path = process.cwd() + '/';
+
+// require('nw.gui').Window.get().showDevTools();
 
 $(function(){
 
   $('#clickable').on('click', function(){
     $('#fileUpload').click();
   });
+
+  $('#fileUpload').on('change',function(event){
+
+    console.log($('#fileUpload').get(0).files[0])
+    var filename = $('#fileUpload').get(0).files[0].name;
+    console.log(filename);
+    var dummyData = {
+      key: 'pair',
+      key2: 'pair2',
+      nested: {
+        key3: 'pair'
+      }
+    };
+
+    // fs.readFile(path + 'proyects/' + filename, JSON.stringify(dummyData), function(err){
+    //   if (err){
+    //     alert(err);
+    //   } else {
+    //     alert('no error')
+    //   }
+    // });
+
+  })
 
 
 });
