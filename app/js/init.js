40fdf2b605ae1e01add815590aeec3740aced442
Roberto Alvarez <robertoatc02@gmail.com>
2014-05-27 12:56:32 -0700

    Refactors save & load event listeners for LZADialog

--- app/js/init.js
+++ app/js/init.js
@@ -1,17 +1,8 @@
 "use strict";
-// var fs = require('fs');
-// var path = require('path');
-// console.log('---')
-// var path = process.cwd() + '/';
-// var path = require('path');
+
 require('nw.gui').Window.get().showDevTools();
 var fs = require('fs');
-// var lzadialog = require('./lzadialog.js');
-// alert(path.dirname(process.execPath))
-// console.log(process)
-// console.log(process.cwd())
 
-// var proyectPath = process.cwd() + '/proyects/';
 
 $(function(){
 
@@ -56,37 +47,26 @@ $(function(){
     });
   });
 
-  $('#fileSave').on('click', function(){
-    var dir = $('#fileSave').get(0);
-    console.log(dir);
-  })
-
   $('#loadFile').on('click', function(){
-    $('#fileUpload').click();
+    LZADialog.selectFile({}, function(file){
+      var path = file.path;
+      var filename = file.name;
+      var dir = path.replace(filename,'');
+      console.log(file);
+      var data = new FileReader();
+      data.readAsText(file);
+
+      data.onloadend = function() {
+        try {
+          var fileContents = JSON.parse(data.result);
+          alert('Successfuly read file');
+          console.log(fileContents);
+        } 
+        catch (err) {
+          alert('File is unreadable, make sure you selected the correct JSON file');
+        }
+      }; 
+    });
   });
-
-  $('#fileUpload').on('change',function(){
-
-    var file = $('#fileUpload').get(0).files[0];
-    var filename = file.name;
-    var filepath = file.path;
-    filepath = filepath.replace(filename,'');
-
-    var data = new FileReader();
-    data.readAsText(file);
-
-    data.onloadend = function() {
-      try {
-        var fileContents = JSON.parse(data.result);
-        alert('Successfuly read file');
-        console.log(fileContents);
-      } 
-      catch (err) {
-        alert('File is unreadable, make sure you selected the correct JSON file');
-      }
-    }; 
-  });
-
-
 });
 
